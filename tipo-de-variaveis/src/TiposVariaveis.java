e9c79b70445ecb88d9ca8da23eb84caaa513d384
esmeraldo17 <esmeraldoalberto55@gmail.com>
2024-06-08 07:03:23 +0100

    study: constants

--- tipo-de-variaveis/src/TiposVariaveis.java
+++ tipo-de-variaveis/src/TiposVariaveis.java
@@ -1,11 +1,20 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
+        //variaveis
         byte idade = 123;
         short ano = 2024;
         int cep = 828728372;
         long cfe = 329393993939393939L;
-        float pi = 3.14F;
+        
         double salario = 2574.99;
 
+        cep = 37;
+        System.out.print(cep);
+
+        // constantes 
+        // usar a keyword final e sempre escrever em caixa alta
+
+        final float PI = 3.14F;
+        System.out.print(PI);
     }
 }
