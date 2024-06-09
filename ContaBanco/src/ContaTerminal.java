596323b9f0a32edc3049b7b0e176badbc6e9dee0
esmeraldo17 <esmeraldoalberto55@gmail.com>
2024-06-09 18:38:03 +0100

    feat: instanciando o scanner, criando os atributos de contaTerminal e imprimi-los no terminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o seu nome !");
+        String nome = scanner.next();
+
+        System.out.println("Por favor, digite o número da Agência !");
+        String numeroAgencia = scanner.next();
+
+        System.out.println("Por favor, digite o número da conta !");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência !");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá "+ nome + " obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + ", conta " + numeroConta +" e seu saldo " + saldo + " já está disponível para saque.");
+
+        scanner.close();
     }
 }
