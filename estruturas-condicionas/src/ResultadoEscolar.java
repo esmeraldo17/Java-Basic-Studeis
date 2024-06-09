e5c158178fcdaf650131650e60cac08d8c50edce
esmeraldo17 <esmeraldoalberto55@gmail.com>
2024-06-09 20:04:51 +0100

    study: operador ternario

--- estruturas-condicionas/src/ResultadoEscolar.java
+++ estruturas-condicionas/src/ResultadoEscolar.java
@@ -1,12 +1,16 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 5;
+        int nota = 9;
 
-        if(nota >= 7)
-            System.out.println("Aprovado");
-        else if(nota >= 5 && nota < 7)
-            System.out.println("Recuperacao");
-        else
-            System.out.println("Reprovado");
+        // if(nota >= 7)
+        //     System.out.println("Aprovado");
+        // else if(nota >= 5 && nota < 7)
+        //     System.out.println("Recuperacao");
+        // else
+        //     System.out.println("Reprovado");
+
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+
+        System.out.println(resultado);
     }
 }
