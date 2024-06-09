4529ebc3182c14cc64ed68add00736002bd58bbc
esmeraldo17 <esmeraldoalberto55@gmail.com>
2024-06-09 19:59:35 +0100

    study: condicionais encadeadas

--- estruturas-condicionas/src/ResultadoEscolar.java
+++ estruturas-condicionas/src/ResultadoEscolar.java
@@ -1,9 +1,11 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 8;
+        int nota = 5;
 
         if(nota >= 7)
             System.out.println("Aprovado");
+        else if(nota >= 5 && nota < 7)
+        System.out.println("Recuperacao");
         else
             System.out.println("Reprovado");
     }
