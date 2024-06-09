27ba13b7650ff9f6a658a0bec181d1af8642f35b
esmeraldo17 <esmeraldoalberto55@gmail.com>
2024-06-09 12:38:40 +0100

    Study: operadores unarios

--- operadores/src/App.java
+++ operadores/src/App.java
@@ -7,6 +7,24 @@ public class App {
         int multiplicacao = 4 * 6;
         int divisao = 80 / 20;
         int modulo = 10 % 8;
-        
+
+
+        // Operadores unarios
+
+        int negNum = 5;
+
+        negNum = -negNum;
+        System.out.println(negNum);
+
+        int posNum = negNum * -1; // forma de tornar um numero positivo
+        System.out.println(posNum);
+
+        posNum++; // incremento
+        System.out.println(posNum);
+        System.out.println(++ posNum);
+
+        posNum--; // decremento
+        System.out.println(posNum);
+        System.out.println(-- posNum);
     }
 }
