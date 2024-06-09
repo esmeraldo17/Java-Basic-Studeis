671d546930f02174dbb9b29842a87428f5fef123
esmeraldo17 <esmeraldoalberto55@gmail.com>
2024-06-09 12:19:59 +0100

    study: aritmetic operators

--- operadores/src/App.java
+++ operadores/src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Operadores aritmeticos
+
+        int adicao = 34 + 56;
+        int subtracao = 1500 - 499;
+        int multiplicacao = 4 * 6;
+        int divisao = 80 / 20;
+        int modulo = 10 % 8;
+        
     }
 }
