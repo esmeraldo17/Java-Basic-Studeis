6198856e89211cb76680e8e18341e04564d09fe7
esmeraldo17 <esmeraldoalberto55@gmail.com>
2024-06-09 20:07:21 +0100

    study: operador ternario encadeado

--- estruturas-condicionas/src/ResultadoEscolar.java
+++ estruturas-condicionas/src/ResultadoEscolar.java
@@ -1,6 +1,6 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 9;
+        int nota = 8;
 
         // if(nota >= 7)
         //     System.out.println("Aprovado");
@@ -9,7 +9,12 @@ public class ResultadoEscolar {
         // else
         //     System.out.println("Reprovado");
 
-        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        // String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+
+        // System.out.println(resultado);
+
+
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperacao" : "Reprovado";
 
         System.out.println(resultado);
     }
