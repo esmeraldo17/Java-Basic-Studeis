d7fa470f2060d62912da19c19744454e0a602abd
esmeraldo17 <esmeraldoalberto55@gmail.com>
2024-06-10 06:01:44 +0100

    study: estruturas de repeticao for em arrays

--- estruturas-repeticoes/src/ExemploForArray.java
+++ estruturas-repeticoes/src/ExemploForArray.java
@@ -2,8 +2,12 @@ public class ExemploForArray {
     public static void main(String[] args) {
         String alunos[] = {"Esmeraldo","Edon","Carlos","Antonio"};
 
-        for (int x = 0; x < alunos.length; x++){
-            System.out.println("o alunos no indice x=" + x + " e " + alunos[x]);
+        // for (int x = 0; x < alunos.length; x++){
+        //     System.out.println("o alunos no indice x=" + x + " e " + alunos[x]);
+        // }
+        // for each: usado para iteragir sobre os elementos de um array
+        for (String aluno : alunos){
+            System.out.println("Nome do aluno e: " + aluno);
         }
     }
 }
