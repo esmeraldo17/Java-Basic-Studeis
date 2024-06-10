95dfa7cab18e0e31185ddff4156bb21f4bbb3a1a
esmeraldo17 <esmeraldoalberto55@gmail.com>
2024-06-10 05:59:09 +0100

    study: estrutura de repeticoes for com arrays

---
+++ estruturas-repeticoes/src/ExemploForArray.java
@@ -0,0 +1,9 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos[] = {"Esmeraldo","Edon","Carlos","Antonio"};
+
+        for (int x = 0; x < alunos.length; x++){
+            System.out.println("o alunos no indice x=" + x + " e " + alunos[x]);
+        }
+    }
+}
